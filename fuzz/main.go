98cdedf5e99ad1f7db8f6f2082c6b876afa58091
Himanshu Singh <hsnice16@gmail.com>
2024-08-04 01:29:57 +0530

    Fix the byte conversion bug

--- fuzz/main.go
+++ fuzz/main.go
@@ -1,21 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"unicode/utf8"
+)
 
 func main() {
 	input := "The quick brown fox jumped over the lazy dog"
-	rev := Reverse(input)
-	doubleRev := Reverse(rev)
+	rev, revError := Reverse(input)
+	doubleRev, doubleRevError := Reverse(rev)
 
 	fmt.Printf("original: %q\n",input)
-	fmt.Printf("reversed: %q\n",rev)
-	fmt.Printf("reversed again: %q\n",doubleRev)
+	fmt.Printf("reversed: %q, err: %v\n",rev, revError)
+	fmt.Printf("reversed again: %q, err: %v\n",doubleRev, doubleRevError)
 }
 
-func Reverse(s string) string {
-	b := []byte(s)
-	for i, j := 0, len(b)-1; i < len(b)/2; i,j = i+1, j-1 {
-		b[i], b[j] = b[j], b[i]
+func Reverse(s string) (string, error) {
+	if !utf8.ValidString(s) {
+		return s, errors.New("input is not valid UTF-8")
 	}
-	return string(b)
+
+	r := []rune(s)
+	for i, j := 0, len(r)-1; i < len(r)/2; i,j = i+1, j-1 {
+		r[i], r[j] = r[j], r[i]
+	}
+	return string(r), nil
 }
--- fuzz/reverse_test.go
+++ fuzz/reverse_test.go
@@ -8,15 +8,24 @@ import (
 	
 
 func FuzzReverse(f *testing.F) {
-	testcases := []string {"Hello, world", "dlrow ,olleH"}
+	testcases := []string {"Hello, world", " ", "!12345", "😅"}
 	for _, tc := range testcases {
 		f.Add(tc)		// Use f.Add to provide a seed corpus
 	}
 
 
 	f.Fuzz(func(t *testing.T, orig string) {
-		rev := Reverse(orig)
-		doubleRev := Reverse(rev)
+		rev, err1 := Reverse(orig)
+		if err1 != nil {
+			return
+		}
+
+		doubleRev, err2 := Reverse(rev)
+		if err2 != nil {
+			return
+		}
+
+		t.Logf("Number of runes: orig=%d, rev=%d, doubleRev=%d", utf8.RuneCountInString(orig), utf8.RuneCountInString(rev),utf8.RuneCountInString(doubleRev))
 		
 		if orig != doubleRev {
 			t.Errorf("Before: %q, after: %q", orig, doubleRev)
