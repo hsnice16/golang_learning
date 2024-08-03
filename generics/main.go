7b53750f4d70395a1a879283a40ed2719e88c870
Himanshu Singh <hsnice16@gmail.com>
2024-08-03 21:02:05 +0530

    Remove type arguments when calling

--- generics/main.go
+++ generics/main.go
@@ -18,6 +18,9 @@ func main() {
 	fmt.Printf("Non-Generic Sums: %v and %v\n",SumInts(ints), SumFloats(floats))
 
 	fmt.Printf("Generic Sums: %v and %v\n", SumIntsOrFloats[string, int64](ints), SumIntsOrFloats[string, float64](floats))
+
+	fmt.Printf("Generic Sums, type parameters inferred: %v and %v\n", SumIntsOrFloats(ints), SumIntsOrFloats(floats))
+
 }
 
 // SumInts adds together the values of m.
