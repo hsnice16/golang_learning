ca5fae8c1e0ed72ba4c7c5b82f21118253caf9f7
Himanshu Singh <hsnice16@gmail.com>
2024-08-04 14:54:50 +0530

    net/http to serve wiki pages

--- gowiki/wiki.go
+++ gowiki/wiki.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 	"os"
 )
 
@@ -20,6 +22,9 @@ func main() {
 	p1.save()
 	p2, _ := loadPage("TestPage")
 	fmt.Println(string(p2.Body))
+
+	http.HandleFunc("/view/",viewHandler)
+	log.Fatal(http.ListenAndServe(":8080",nil))
 }
 
 func loadPage(title string) (*Page, error) {
@@ -29,4 +34,10 @@ func loadPage(title string) (*Page, error) {
 		return nil, err
 	}
 	return &Page{Title: title, Body: body}, nil
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	title  := r.URL.Path[len("/view/"):]
+	p, _ := loadPage(title)
+	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
 }
