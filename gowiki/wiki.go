26f8f3e1b01018721355ec72ea54e406e47fc476
Himanshu Singh <hsnice16@gmail.com>
2024-08-04 16:38:09 +0530

    edit page

--- gowiki/wiki.go
+++ gowiki/wiki.go
@@ -24,6 +24,8 @@ func main() {
 	fmt.Println(string(p2.Body))
 
 	http.HandleFunc("/view/",viewHandler)
+	http.HandleFunc("/edit/",editHandler)
+	// http.HandleFunc("/save/",saveHandler)
 	log.Fatal(http.ListenAndServe(":8080",nil))
 }
 
@@ -40,4 +42,13 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 	title  := r.URL.Path[len("/view/"):]
 	p, _ := loadPage(title)
 	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/edit/"):]
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+	fmt.Fprintf(w, "<h1>Editing %s</h1>"+"<form action=\"/save/%s\" method=\"POST\">" + "<textarea name=\"body\">%s</textarea><br>" + "<input type=\"submit\" value=\"Save\">" + "</form>",p.Title, p.Title, p.Body)
 }
