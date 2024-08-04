54659a3fd3501fde7b3899d60aae7bb68abb51cc
Himanshu Singh <hsnice16@gmail.com>
2024-08-04 16:56:09 +0530

    save page

--- gowiki/wiki.go
+++ gowiki/wiki.go
@@ -26,7 +26,7 @@ func main() {
 
 	http.HandleFunc("/view/",viewHandler)
 	http.HandleFunc("/edit/",editHandler)
-	// http.HandleFunc("/save/",saveHandler)
+	http.HandleFunc("/save/",saveHandler)
 	log.Fatal(http.ListenAndServe(":8080",nil))
 }
 
@@ -65,4 +65,12 @@ func editHandler(w http.ResponseWriter, r *http.Request) {
 func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
 	t, _ := template.ParseFiles(tmpl + ".html")
 	t.Execute(w, p)
+}
+
+func saveHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/save/"):]
+	body := r.FormValue("body")
+	p := &Page{Title: title, Body: []byte(body)}
+	p.save()
+	http.Redirect(w, r, "/view/"+title, http.StatusFound)
 }
