049ef47ba6d46917c7d3e5fc078635c458d70cf4
Himanshu Singh <hsnice16@gmail.com>
2024-08-21 11:42:18 +0530

    Parse JSON to interface

--- text_files/json/main.go
+++ text_files/json/main.go
@@ -19,4 +19,29 @@ func main() {
 	str := `{"servers":[{"serverName":"Shanghai_VPN","serverIP":"127.0.0.1"},{"serverName":"Beijing_VPN","serverIP":"127.0.0.2"}]}`
 	json.Unmarshal([]byte(str), &s)
 	fmt.Println(s)
+
+	b := []byte(`{"Name":"Wednesday","Age":6,"Parents":["Gomez","Morticia"]}`)
+	var f interface{}
+	json.Unmarshal(b, &f)
+	fmt.Println(f)
+
+	m := f.(map[string]interface{}) // Access the data with Type assertion
+
+	for k, v := range m {
+		switch vv := v.(type) {
+		case string:
+			fmt.Println(k, "is string", vv)
+		case int:
+			fmt.Println(k, "is int", vv)
+		case float64:
+			fmt.Println(k, "is float64", vv)
+		case []interface{}:
+			fmt.Println(k, "is an array")
+			for i, u := range vv {
+				fmt.Println(i, u)
+			}
+		default:
+			fmt.Println(k, "is of a type I don't know how to handle")
+		}
+	}
 }
