23ba2f6c5a54a34ead98754f95751c59b614d164
Himanshu Singh <hsnice16@gmail.com>
2024-08-24 12:03:25 +0530

    Templates -- Conditions

--- text_files/templates/main.go
+++ text_files/templates/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"os"
 )
@@ -31,4 +32,18 @@ func main() {
 
 	p := Person{UserName: "Himanshu", Emails: []string{"hs@bee.me", "astax@example.com"}, Friends: []*Friend{&f1, &f2}}
 	t.Execute(os.Stdout, p)
+
+	fmt.Println("--------------- Conditions ------------------")
+
+	tEmpty := template.New("template test")
+	tEmpty = template.Must(tEmpty.Parse("Empty pipeline if demo: {{if ``}} will not be outputted. {{end}}\n"))
+	tEmpty.Execute(os.Stdout, nil)
+
+	tWithValue := template.New("template test")
+	tWithValue = template.Must(tWithValue.Parse("Not empty pipeline if demo: {{if `anything`}} will be outputted. {{end}}\n"))
+	tWithValue.Execute(os.Stdout, nil)
+
+	tIfElse := template.New("template test")
+	tIfElse = template.Must(tIfElse.Parse("if-else demo: {{if `anything`}} if part {{else}} else part.{{end}}\n"))
+	tIfElse.Execute(os.Stdout, nil)
 }
