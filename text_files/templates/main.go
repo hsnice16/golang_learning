0667a985879963775380931a673b30f241d88bbc
Himanshu Singh <hsnice16@gmail.com>
2024-08-24 12:44:10 +0530

    Templates -- Nested templates

--- text_files/templates/main.go
+++ text_files/templates/main.go
@@ -3,17 +3,39 @@ package main
 import (
 	"fmt"
 	"html/template"
+	"io/ioutil"
+	"os"
+	"strings"
 )
 
+var templates *template.Template
+
 func main() {
-	tOk := template.New("first")
-	template.Must(tOk.Parse(" some static text /* and a comment */"))
-	fmt.Println("The first one parsed Ok.")
+	var allFiles []string
+	files, err := ioutil.ReadDir("./raw_templates")
+	if err != nil {
+		fmt.Println(err)
+	}
+	for _, file := range files {
+		filename := file.Name()
+		if strings.HasSuffix(filename, ".tmpl") {
+			allFiles = append(allFiles, "./raw_templates/"+filename)
+		}
+	}
 
-	template.Must(template.New("second").Parse("some static text {{ .Name }}"))
-	fmt.Println("The second one parsed OK.")
+	templates, err = template.ParseFiles(allFiles...) // parses all .tmpl files in the 'raw_templates' folder
 
-	fmt.Println("The next one ought to fail.")
-	tErr := template.New("check parse error with Must")
-	template.Must(tErr.Parse(" some static text {{ .Name }"))
+	s1 := templates.Lookup("header.tmpl")
+	s1.ExecuteTemplate(os.Stdout, "header", nil)
+	fmt.Println()
+
+	s2 := templates.Lookup("content.tmpl")
+	s2.ExecuteTemplate(os.Stdout, "content", nil)
+	fmt.Println()
+
+	s3 := templates.Lookup("footer.tmpl")
+	s3.ExecuteTemplate(os.Stdout, "footer", nil)
+	fmt.Println()
+
+	s3.Execute(os.Stdout, nil)
 }
