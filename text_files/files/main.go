6aa5a4eaa674a15fb1ba10c86ed2581f3d0e6703
Himanshu Singh <hsnice16@gmail.com>
2024-08-25 10:43:01 +0530

    Files -- Read File

--- text_files/files/main.go
+++ text_files/files/main.go
@@ -15,14 +15,29 @@ func main() {
 	os.RemoveAll("hsnice16")
 
 	userFile := "hsnice16.txt"
-	fout, err := os.Create(userFile)
+	// fout, err := os.Create(userFile)
+	// if err != nil {
+	// 	fmt.Println(userFile, err)
+	// 	return
+	// }
+	// defer fout.Close()
+	// for i := 0; i < 10; i++ {
+	// 	fout.WriteString("Just a test!\r\n")
+	// 	fout.Write([]byte("Just a test!\r\n"))
+	// }
+
+	fl, err := os.Open(userFile)
 	if err != nil {
 		fmt.Println(userFile, err)
 		return
 	}
-	defer fout.Close()
-	for i := 0; i < 10; i++ {
-		fout.WriteString("Just a test!\r\n")
-		fout.Write([]byte("Just a test!\r\n"))
+	defer fl.Close()
+	buf := make([]byte, 1024)
+	for {
+		n, _ := fl.Read(buf)
+		if 0 == n {
+			break
+		}
+		os.Stdout.Write(buf[:n])
 	}
 }
