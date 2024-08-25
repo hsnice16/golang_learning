483f5a311cf71fb41c7790dba9e87a87bbbf020b
Himanshu Singh <hsnice16@gmail.com>
2024-08-25 10:39:52 +0530

    Files -- Create & Write File

--- text_files/files/main.go
+++ text_files/files/main.go
@@ -13,4 +13,16 @@ func main() {
 		fmt.Println(err)
 	}
 	os.RemoveAll("hsnice16")
+
+	userFile := "hsnice16.txt"
+	fout, err := os.Create(userFile)
+	if err != nil {
+		fmt.Println(userFile, err)
+		return
+	}
+	defer fout.Close()
+	for i := 0; i < 10; i++ {
+		fout.WriteString("Just a test!\r\n")
+		fout.Write([]byte("Just a test!\r\n"))
+	}
 }
