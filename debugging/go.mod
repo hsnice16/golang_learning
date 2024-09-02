02c7235a8dc20122607c0b6377ab3e7e8e12e483
Himanshu Singh <hsnice16@gmail.com>
2024-09-02 10:07:50 +0530

    Debugging -- use GDB

---
+++ debugging/go.mod
@@ -0,0 +1,3 @@
+module github.com/hsnice16/golang_learning/debugging
+
+go 1.22.6
---
+++ debugging/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func counting(c chan<- int) {
+	for i := 0; i < 10; i++ {
+		time.Sleep(2 * time.Second)
+		c <- i
+	}
+	close(c)
+}
+
+func main() {
+	msg := "Starting main"
+	fmt.Println(msg)
+	bus := make(chan int)
+	msg = "starting a gofunc"
+	go counting(bus)
+	for count := range bus {
+		fmt.Println("count:", count)
+	}
+}
